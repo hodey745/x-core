453d4c9ed223a98020d8035b63198f26dc124839
X-Core <63533170+hodey745@users.noreply.github.com>
2020-06-04 00:15:00 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,9 +4,12 @@ pkg install git
 pkg install python2
 pkg install python
 pkg install php
-pkg install requests
-pkg install mechanize
+pip install requests
+pip2 install requests
+pip install mechanize
+pip2 install mechanize
 pkg install ruby
+pkg install figlet
 gem install lolcat
 chmod 777 hui.sh
 sh hui.sh
